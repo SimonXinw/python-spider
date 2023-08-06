88a16bf77ec6f7039afecaff412f0b8277d6370f
辛望 <1294600058@qq.com>
2023-08-07 03:40:19 +0800

    feat: 创建一个 类

--- IP-获取域名对应的IP/main.py
+++ IP-获取域名对应的IP/main.py
@@ -1,54 +1,38 @@
 import socket
 import os
+from config import DOMAINS
 
 
-def output_hosts():
-    domains = ['github.com',
-               'gist.github.com',
-               'assets-cdn.github.com',
-               'raw.githubusercontent.com',
-               'gist.githubusercontent.com',
-               'cloud.githubusercontent.com',
-               'camo.githubusercontent.com',
-               'avatars0.githubusercontent.com',
-               'avatars1.githubusercontent.com',
-               'avatars2.githubusercontent.com',
-               'avatars3.githubusercontent.com',
-               'avatars4.githubusercontent.com',
-               'avatars5.githubusercontent.com',
-               'avatars6.githubusercontent.com',
-               'avatars7.githubusercontent.com',
-               'avatars8.githubusercontent.com',
-               'avatars.githubusercontent.com',
-               'github.githubassets.com',
-               'user-images.githubusercontent.com',
-               'codeload.github.com',
-               'favicons.githubusercontent.com',
-               'api.github.com'
-               ]
+class QueryIp:
+    def __init__(self) -> None:
+        pass
 
-    # 获取当前文件夹的绝对路径
-    current_folder = os.getcwd()
+    def output_hosts():
+        domains = DOMAINS
 
-    # 获取当前文件夹同级的 output_images 文件夹的绝对路径
-    SAVE_FILE_PATH = os.path.abspath(os.path.join(
-        current_folder, 'IP-获取域名对应的IP', 'hosts.txt'))
+        # 获取当前文件夹的绝对路径
+        current_folder = os.getcwd()
 
-    with open(SAVE_FILE_PATH, 'w') as f:
-        f.write('# GitHub Start \n')
+        # 获取当前文件夹同级的 output_images 文件夹的绝对路径
+        SAVE_FILE_PATH = os.path.abspath(os.path.join(
+            current_folder, 'IP-获取域名对应的IP', 'hosts.txt'))
 
-        for domain in domains:
-            ip = socket.gethostbyname(domain)
+        with open(SAVE_FILE_PATH, 'w') as f:
+            f.write('# GitHub Start \n')
 
-            print('Querying ip for domain %s' % domain + ':' + ip)
+            for domain in domains:
+                ip = socket.gethostbyname(domain)
 
-            f.write('%s %s\n' % (ip, domain))
+                print('Querying ip for domain %s' % domain + ':' + ip)
 
-            f.write('\n')
+                f.write('%s %s\n' % (ip, domain))
 
-        # 结尾
-        f.write('# GitHub End \n')
+                f.write('\n')
+
+            # 结尾
+            f.write('# GitHub End \n')
 
 
 if __name__ == '__main__':
-    output_hosts()
+    query = QueryIp()
+    query.output_hosts()
