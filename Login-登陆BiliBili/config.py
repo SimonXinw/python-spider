e6dd76ab62172f6a0b53b2ff21212253f331b734
辛望 <1294600058@qq.com>
2023-08-25 00:04:00 +0800

    feat: 登陆代码写完了

---
+++ Login-登陆BiliBili/config.py
@@ -0,0 +1,6 @@
+# 网站 URL
+PAGE_URL = 'https://www.bilibili.com/'
+
+USER = 18270515107
+
+PASSWORD = 'xinwang1997'
---
+++ Login-登陆BiliBili/main.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from config import USER, PAGE_URL, PASSWORD
+
+
+class WebOperator:
+    def __init__(self, base_url, user, password):
+        self.base_url = base_url
+
+        self.user = user
+
+        self.password = password
+
+        driver_options = webdriver.ChromeOptions()  # 谷歌选项
+
+        # 设置 User-Agent 头
+        driver_options.add_argument(
+            'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36')
+
+        # 设置为开发者模式，避免被识别
+        driver_options.add_experimental_option('excludeSwitches',
+                                               ['enable-automation'])
+
+        self.driver = webdriver.Chrome(options=driver_options)
+
+        self.wait = WebDriverWait(self.driver, 2)
+
+    def crawling(self):
+        self.driver.get(self.base_url)
+
+        # 等待页面加载完成，设置最长等待时间为 10 秒
+        wait = WebDriverWait(self.driver, 10)
+
+        # 等待元素渲染出来之后取页面 html 数据
+        wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '.go-login-btn')))
+
+        #  获取输入框 dom 节点
+        login_btn = self.driver.find_element(By.CSS_SELECTOR, '.go-login-btn')
+
+        login_btn.click()
+
+        # 等待元素渲染出来之后取页面 html 数据
+        wait.until(EC.presence_of_element_located(
+            (By.CLASS_NAME, 'login-pwd-wp')))
+
+        # 找到 form__item 元素
+        login_wrap_ele = self.driver.find_element(
+            By.CLASS_NAME, 'login-pwd-wp')
+
+        # 找到带有指定 placeholder 属性的 input 元素
+        user_ele = login_wrap_ele.find_element(
+            By.XPATH, './/input[@placeholder="请输入账号"]')
+
+        password_ele = login_wrap_ele.find_element(
+            By.XPATH, './/input[@placeholder="请输入密码"]')
+
+        login_btn_ele = login_wrap_ele.find_element(
+            By.CLASS_NAME, 'btn_primary')
+
+        user_ele.clear()
+
+        password_ele.clear()
+
+        #  搜索商品
+        user_ele.send_keys(self.user)
+
+        password_ele.send_keys(self.password)
+
+        login_btn_ele.click()
+
+        self.driver.quit()
+
+
+scraper = WebOperator(PAGE_URL, USER, PASSWORD)
+
+scraper.crawling()
