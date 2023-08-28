07364dd0a2f75c9ba0dbd256748546d79aa8dfb4
xinwang <wb-xw818042@alibaba-inc.com>
2023-08-28 14:08:05 +0800

    feat: 真的可以识别 orc 文字字符

--- Login-登陆BiliBili/config.py
+++ Login-登陆BiliBili/config.py
@@ -1,6 +1,30 @@
 # 网站 URL
+import os
+from utils import common
+
+utils_instance = common.Utils()
+
 PAGE_URL = 'https://www.bilibili.com/'
 
 USER = 18270515107
 
 PASSWORD = 'xinwang1997'
+
+
+# 获取当前文件夹的绝对路径
+current_folder = os.getcwd()
+
+# 获取当前文件夹同级的 output_images 文件夹的绝对路径
+img_dir_path = os.path.abspath(os.path.join(
+    current_folder, 'Login-登陆BiliBili', 'verification_code'))
+
+file_name = utils_instance.get_first_filename(img_dir_path)
+
+# excel 文件绝对路径 path
+file_abs_path = os.path.abspath(
+    os.path.join(img_dir_path, file_name))
+
+# 网站 URL
+PAGE_URL = ''
+
+COOKIES = ''
--- Login-登陆BiliBili/orc.py
+++ Login-登陆BiliBili/orc.py
@@ -1,10 +1,14 @@
 import pytesseract
 from PIL import Image
+from config import file_abs_path
 
 
 def ocr_code():
 
     # open image
-    image = Image.open('test.png')
+    image = Image.open(file_abs_path)
     code = pytesseract.image_to_string(image, lang='chi_sim')
-    print(code)
+    print('输出字符: ' + code)
+
+
+ocr_code()
