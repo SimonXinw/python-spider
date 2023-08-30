6a18c23f21ee46c6cbce18fb10edf7346a505186
xinwang <wb-xw818042@alibaba-inc.com>
2023-08-30 10:20:52 +0800

    feat: 进行了再次的封装

--- Login-登陆BiliBili/config.py
+++ Login-登陆BiliBili/config.py
@@ -28,6 +28,15 @@ file_name = utils_instance.get_first_filename(img_dir_path)
 file_abs_path = os.path.abspath(
     os.path.join(img_dir_path, file_name))
 
+# 验证码文件绝对路径 path
+code_img_abs_path = os.path.abspath(
+    os.path.join(img_dir_path, CODE_IMG_NAME))
+
+# 验证码的顺序文件绝对路径 path
+order_img_abs_path = os.path.abspath(
+    os.path.join(img_dir_path, ORDER_IMG_NAME))
+
+
 # 网站 URL
 PAGE_URL = 'https://www.bilibili.com/'
 
---
+++ Login-登陆BiliBili/orc/daidai_didi.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@Project :demo 
+@File    :test_ocr.py
+@Author  :lijiawei
+@Date    :2021/8/10 8:45 上午 
+"""
+import ddddocr
+
+
+def orc_img(img_abs_path):
+    ocr = ddddocr.DdddOcr()
+
+    with open(img_abs_path, 'rb') as f:
+        image = f.read()
+
+    res = ocr.classification(image)
+
+    return res
+
+
+# orc_img('/Users/xinwang/Desktop/xw/python/python-spider/Login-登陆BiliBili/verification_code/code.png')
